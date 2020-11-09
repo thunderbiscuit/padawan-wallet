1afe2ea40cac5beb9c476a62a341d9e1726993e1
thunderbiscuit <thunderB@protonmail.com>
2020-11-09 16:49:36 -0500

    Fix app ID

--- app/src/main/java/com/grizzlysoftware/padawanwallet/MainActivity.kt
+++ app/src/main/java/com/grizzlysoftware/padawanwallet/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.padawanwallet
+package com.grizzlysoftware.padawanwallet
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.grizzlysoftware.padawanwallet.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
