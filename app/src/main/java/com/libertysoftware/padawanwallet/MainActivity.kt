ed6078d465e5568cf289d7467ff64d8682362b4c
thunderbiscuit <thunderB@protonmail.com>
2020-11-18 17:56:07 -0500

    Prepare for ViewPager2

--- app/src/main/java/com/libertysoftware/padawanwallet/MainActivity.kt
+++ app/src/main/java/com/libertysoftware/padawanwallet/MainActivity.kt
@@ -4,20 +4,30 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    // lateinit var tabLayout: TabLayout
+    // lateinit var viewPager: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//    }
-//
-//    fun showTutorialsFragment(view: View) {
-//        Navigation.findNavController(view).navigate(R.id.action_walletFragment_to_tutorialsFragment)
-//    }
-//
-//    fun showWalletFragment(view: View) {
-//        Navigation.findNavController(view).navigate(R.id.action_tutorialsFragment_to_walletFragment)
-//    }
+        // tabLayout = findViewById(R.id.tabLayout)
+        // viewPager = findViewById(R.id.pager)
+        // viewPager.adapter = AppViewPagerAdapter(supportFragmentManager, lifecycle)
+
+//        TabLayoutMediator(tabLayout, viewPager, TabLayoutMediator.TabConfigurationStrategy { tab, position ->
+//            when (position) {
+//                0 -> { tab.text = "TAB ONE" }
+//                1 -> { tab.text = "TAB TWO" }
+//            }
+//        }).attach()
+
+//        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+//            when (position) {
+//                0 -> { tab.text = "TAB ONE" }
+//                1 -> { tab.text = "TAB TWO" }
+//            }
+//        }.attach()
+    }
 }
