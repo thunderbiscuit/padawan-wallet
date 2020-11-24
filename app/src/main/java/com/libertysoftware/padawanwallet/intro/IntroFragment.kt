209d0b6d90d21d7dd45c1a6a4cf4c64bb92c4868
thunderbiscuit <thunderB@protonmail.com>
2020-11-23 22:38:12 -0500

    Add intro dialog MVP

--- app/src/main/java/com/libertysoftware/padawanwallet/intro/IntroFragment.kt
+++ app/src/main/java/com/libertysoftware/padawanwallet/intro/IntroFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.libertysoftware.padawanwallet.R
@@ -24,9 +25,31 @@ class IntroFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val navController = Navigation.findNavController(view)
-        view.findViewById<Button>(R.id.button1).setOnClickListener {
-            navController!!.navigate(R.id.action_introFragment_to_walletChoice)
-        }
-    }
 
+        val testnetDialog = this@IntroFragment.context?.let {
+            AlertDialog.Builder(it)
+                .setTitle("Be Careful!")
+                .setMessage("It’s important you know that Padawan is not a bitcoin wallet that can handle normal bitcoins!\n" +
+                        "\n" +
+                        "This wallet is built to help you learn how to use bitcoin wallets through a series of tutorials, and it does so using testnet coins, a type of bitcoin that doesn’t have any value.\n" +
+                        "\n" +
+                        "The wallet can only handle testnet coins, so make sure you don’t send it normal coins!")
+                    .setPositiveButton("I understand") { _, _ ->
+                        navController!!.navigate(R.id.action_introFragment_to_walletChoice)
+                    }
+                    .setNegativeButton("Cancel") { _, _ ->
+                        // Toast.makeText(this!!, "Read this page again!", Toast.LENGTH_LONG).show()
+                        // log cancel
+                    }
+        }
+        // view.findViewById<Button>(R.id.button5).setOnClickListener {
+        //     testnetDialog?.show()
+        // }
+
+        view.findViewById<Button>(R.id.button1).setOnClickListener {
+            // navController!!.navigate(R.id.action_introFragment_to_walletChoice)
+            testnetDialog?.show()
+        }
+
+    }
 }
