4bb67251e20f943286d1c57a1845405e5e2548bc
thunderbiscuit <thunderB@protonmail.com>
2020-12-19 10:47:51 -0500

    Refactor: Use view binding in wallet fragment

--- app/src/main/java/com/libertysoftware/padawanwallet/home/WalletFragment.kt
+++ app/src/main/java/com/libertysoftware/padawanwallet/home/WalletFragment.kt
@@ -10,16 +10,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.libertysoftware.padawanwallet.R
+import com.libertysoftware.padawanwallet.databinding.FragmentWalletBinding
 
 class WalletFragment : Fragment() {
+
+    private var _binding: FragmentWalletBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wallet, container, false)
+        // return inflater.inflate(R.layout.fragment_wallet, container, false)
+        _binding = FragmentWalletBinding.inflate(inflater, container, false)
+        val view: View = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
