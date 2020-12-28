39b3c96d43bbdf2d83d85a4da84867f7fa5f354c
thunderbiscuit <thunderB@protonmail.com>
2020-12-28 23:46:16 +0000

    Feat: Generate receive address on receive fragment

--- app/src/main/java/com/libertysoftware/padawanwallet/home/WalletReceive.kt
+++ app/src/main/java/com/libertysoftware/padawanwallet/home/WalletReceive.kt
@@ -6,8 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import com.libertysoftware.padawanwallet.PadawanWalletApplication
 import com.libertysoftware.padawanwallet.R
 import com.libertysoftware.padawanwallet.databinding.FragmentWalletReceiveBinding
+import timber.log.Timber
 
 class WalletReceive : Fragment() {
 
@@ -30,5 +32,11 @@ class WalletReceive : Fragment() {
         binding.button4.setOnClickListener {
             navController.navigate(R.id.action_walletReceive_to_walletHome)
         }
+
+        val app = requireActivity().application as PadawanWalletApplication
+        val newAddress: String = app.getNewAddress()
+        Timber.i("[PADAWANLOGS] New deposit address is $newAddress")
+
+        binding.receiveAddress.text = newAddress
     }
 }
