1736563c15031b3d5d7d2bfbb7c436631df084b3
thunderbiscuit <thunderB@protonmail.com>
2020-12-30 22:40:07 +0000

    Feat: Add verify transaction screen

--- app/src/main/java/com/libertysoftware/padawanwallet/home/WalletVerify.kt
+++ app/src/main/java/com/libertysoftware/padawanwallet/home/WalletVerify.kt
@@ -7,16 +7,17 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.libertysoftware.padawanwallet.R
-import com.libertysoftware.padawanwallet.databinding.FragmentWalletSendBinding
+import com.libertysoftware.padawanwallet.databinding.FragmentWalletVerifyBinding
 
-class WalletSend : Fragment() {
+class WalletVerify : Fragment() {
 
-    private lateinit var binding: FragmentWalletSendBinding
+    private lateinit var binding: FragmentWalletVerifyBinding
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentWalletSendBinding.inflate(inflater, container, false)
+        binding = FragmentWalletVerifyBinding.inflate(inflater, container, false)
         val view: View = binding.root
         return view
     }
@@ -26,12 +27,8 @@ class WalletSend : Fragment() {
 
         val navController = Navigation.findNavController(view)
 
-        binding.button5.setOnClickListener {
-            navController.navigate(R.id.action_walletSend_to_walletHome)
-        }
-
-        binding.buttonVerify.setOnClickListener {
-            navController.navigate(R.id.action_walletSend_to_walletVerify)
+        binding.buttonBackTransactionBuilder.setOnClickListener {
+            navController.navigate(R.id.action_walletVerify_to_walletSend)
         }
     }
 }
