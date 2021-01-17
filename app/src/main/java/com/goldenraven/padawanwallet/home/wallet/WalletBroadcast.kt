450ea8c863d5931aa9f6a84ed80c84161c69ca74
thunderbiscuit <thunderB@protonmail.com>
2021-01-17 02:39:49 +0000

    Fix: Add total transaction fee on broadcast fragment
    Fixes #38

--- app/src/main/java/com/goldenraven/padawanwallet/home/wallet/WalletBroadcast.kt
+++ app/src/main/java/com/goldenraven/padawanwallet/home/wallet/WalletBroadcast.kt
@@ -9,12 +9,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.goldenraven.padawanwallet.PadawanWalletApplication
 import com.goldenraven.padawanwallet.R
 import com.goldenraven.padawanwallet.databinding.FragmentWalletBroadcastBinding
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 import org.bitcoindevkit.bdkjni.Types.CreateTxResponse
 import org.bitcoindevkit.bdkjni.Types.RawTransaction
 import org.bitcoindevkit.bdkjni.Types.SignResponse
@@ -42,20 +44,26 @@ class WalletBroadcast : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val fees: Int = 0
-        val total: String = (amount.toInt() + fees).toString()
+
+        verifyTransaction()
+        Timber.i("[PADAWANLOGS] transactionDetails from WalletVerify fragment is: $transactionDetails")
+
+        val fees: Long = transactionDetails.details.fees
+        val total: String = (amount.toLong() + fees).toString()
 
         binding.amountTransacted.text = amount
         binding.fees.text = fees.toString()
         binding.total.text = total
         binding.sendToAddress.text = address
 
-        binding.buttonBroadcastTransaction.setOnClickListener {
-            verifyTransaction()
-            broadcastTransaction()
-        }
-
         val navController = Navigation.findNavController(view)
+
+        binding.buttonBroadcastTransaction.setOnClickListener {
+            // verifyTransaction()
+            broadcastTransaction()
+            // TimeUnit.SECONDS.sleep(4)
+            navController.navigate(R.id.action_walletVerify_to_walletHome)
+        }
 
         binding.buttonBackTransactionBuilder.setOnClickListener {
             navController.navigate(R.id.action_walletVerify_to_walletSend)
@@ -71,7 +79,7 @@ class WalletBroadcast : Fragment() {
 
         try {
             transactionDetails = app.createTransaction(feeRate, addresseesAndAmounts, false, null, null, null)
-            Timber.i("[PADAWANLOGS] transactionDetails from WalletVerify fragment is: $transactionDetails")
+            // Timber.i("[PADAWANLOGS] transactionDetails from WalletVerify fragment is: $transactionDetails")
         } catch (e: Throwable) {
             Timber.i("[PADAWANLOGS] Verify transaction failed: ${e.message}")
             // TODO: Show toast related to error
@@ -88,16 +96,19 @@ class WalletBroadcast : Fragment() {
             val rawTx: RawTransaction = app.extractPsbt(signResponse.psbt)
             val txid: Txid = app.broadcast(rawTx.transaction)
             txidString = txid.toString()
-            Timber.i("[PADAWANLOGS] Transaction was broadcasted! txid: $txid, txidString: $txidString")
+            Timber.i("[PADAWANLOGS] Transaction was broadcast! txid: $txid, txidString: $txidString")
         } catch (e: Throwable) {
             Timber.i("[PADAWANLOGS] ${e.message}")
         }
-        showBroadcastSuccessToast(txid = txidString)
+        // showBroadcastSuccessToast(txid = txidString)
+        showBroadcastSuccessSnackbar(txid = txidString)
     }
 
-    private fun showBroadcastSuccessToast(txid: String) {
-        val text: String = "Transaction was broadcasted successfully\ntxid: $txid"
-        val toast: Toast = Toast.makeText(this@WalletBroadcast.context, text, Toast.LENGTH_LONG)
-        toast.show()
+    private fun showBroadcastSuccessSnackbar(txid: String) {
+        val broadcastSuccessSnackbar = Snackbar.make(requireView(), "Transaction was broadcast successfully!", Snackbar.LENGTH_LONG)
+        broadcastSuccessSnackbar.setTextColor(ContextCompat.getColor(requireActivity(), R.color.fg1))
+        broadcastSuccessSnackbar.view.background = resources.getDrawable(R.drawable.background_toast, null)
+        broadcastSuccessSnackbar.animationMode = BaseTransientBottomBar.ANIMATION_MODE_SLIDE
+        broadcastSuccessSnackbar.show()
     }
 }
