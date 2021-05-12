fbc7330542c27644c3c67949b093bfbd86b5a2c6
thunderbiscuit <thunderB@protonmail.com>
2021-05-12 15:15:49 -0400

    Build: Bump Kotlin version to 1.5.0

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,6 +1,6 @@
 object Versions {
-    const val kotlin = "1.4.10"
-    const val kotlinGradlePlugin = "1.4.10"
+    const val kotlin = "1.5.0"
+    const val kotlinGradlePlugin = "1.5.0"
     const val androidGradlePlugin = "4.2.0"
     const val ktor = "1.5.2"
     const val room = "2.2.6"
