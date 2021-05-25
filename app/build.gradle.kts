e0f175d37d68c6bd1c7a7ca7981a8b005295f8c0
thunderbiscuit <thunderB@protonmail.com>
2021-05-24 21:04:32 -0400

    Build: Remove Kotlin standard library dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,7 +75,6 @@ android {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}")
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("com.google.android.material:material:1.2.1")
--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,5 +1,4 @@
 object Versions {
-    const val kotlin = "1.5.0"
     const val kotlinGradlePlugin = "1.5.0"
     const val androidGradlePlugin = "4.2.0"
     const val ktor = "1.5.2"
