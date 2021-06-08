4b32d2fddb302f752ead2be465f959b10722c020
thunderbiscuit <thunderB@protonmail.com>
2021-06-08 15:56:25 -0400

    Build: Bump version number to v0.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdkVersion(26)
         targetSdkVersion(30)
         versionCode = 1
-        versionName = "v0.6.0"
+        versionName = "v0.7.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "FAUCET_URL", faucetUrl)
         buildConfigField("String", "FAUCET_USERNAME", faucetUsername)
