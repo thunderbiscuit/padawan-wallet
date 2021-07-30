58944dd194c4b68c2da3db58c826f9d6a2279008
thunderbiscuit <thunderbiscuit@protonmail.com>
2021-07-30 10:16:27 -0400

    Build: Bump Android Gradle plugin to v4.2.2

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val kotlinGradlePlugin = "1.5.0"
-    const val androidGradlePlugin = "4.2.0"
+    const val androidGradlePlugin = "4.2.2"
     const val ktor = "1.5.2"
     const val room = "2.2.6"
 }
