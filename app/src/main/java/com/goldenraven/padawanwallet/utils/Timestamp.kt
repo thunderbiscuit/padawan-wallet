322b34808b7976a8549b909de3d31b4632d838ca
thunderbiscuit <thunderbiscuit@protonmail.com>
2021-08-27 11:10:32 -0400

    Docs: Add license notice to Timestamp source code

--- app/src/main/java/com/goldenraven/padawanwallet/utils/Timestamp.kt
+++ app/src/main/java/com/goldenraven/padawanwallet/utils/Timestamp.kt
@@ -1,9 +1,13 @@
+/*
+ * Copyright 2020 thunderbiscuit and contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the ./LICENSE file.
+ */
+
 package com.goldenraven.padawanwallet.utils
 
 import android.text.format.DateFormat
 import org.bitcoindevkit.bdkjni.Types.ConfirmationTime
-import java.util.Calendar
-import java.util.Locale
+import java.util.*
 
 // extension function on the timestamp provided in the TransactionDetails type
 fun ConfirmationTime.timestampToString(): String {
