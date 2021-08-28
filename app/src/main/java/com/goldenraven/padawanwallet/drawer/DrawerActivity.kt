82ad196b780fadc4cf8f0f46cdbc11b601fa5281
thunderbiscuit <thunderbiscuit@protonmail.com>
2021-08-28 09:57:32 -0400

    Refactor: Clean up drawer activity class

--- app/src/main/java/com/goldenraven/padawanwallet/drawer/DrawerActivity.kt
+++ app/src/main/java/com/goldenraven/padawanwallet/drawer/DrawerActivity.kt
@@ -6,7 +6,6 @@
 package com.goldenraven.padawanwallet.drawer
 
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
@@ -49,8 +48,7 @@ class DrawerActivity : AppCompatActivity() {
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_action_name)
 
         // open the right fragment when starting the activity
-        val drawerItem: String? = intent.getStringExtra("drawerItem")
-        when(drawerItem) {
+        when(intent.getStringExtra("drawerItem")) {
             "about" -> supportFragmentManager.beginTransaction().replace(R.id.fragmentAbout, AboutFragment()).commit()
             "settings" -> supportFragmentManager.beginTransaction().replace(R.id.fragmentAbout, SettingsFragment()).commit()
             "seedphrase" -> supportFragmentManager.beginTransaction().replace(R.id.fragmentAbout, SeedPhraseFragment()).commit()
@@ -60,38 +58,32 @@ class DrawerActivity : AppCompatActivity() {
         binding.navViewDrawerActivity.setNavigationItemSelectedListener {
             when(it.itemId) {
                 R.id.about -> {
-                    Log.i("Padalogs","clicked about")
                     supportFragmentManager.beginTransaction().replace(R.id.fragmentAbout, AboutFragment()).commit()
                     binding.drawerActivityLayout.closeDrawer(GravityCompat.START)
                     true
                 }
                 R.id.settings -> {
-                    Log.i("Padalogs","clicked setting")
                     supportFragmentManager.beginTransaction().replace(R.id.fragmentAbout, SettingsFragment()).commit()
                     binding.drawerActivityLayout.closeDrawer(GravityCompat.START)
                     true
                 }
                 R.id.mnemonic -> {
-                    Log.i("Padalogs","clicked seed phrase")
                     supportFragmentManager.beginTransaction().replace(R.id.fragmentAbout, SeedPhraseFragment()).commit()
                     binding.drawerActivityLayout.closeDrawer(GravityCompat.START)
                     true
                 }
                 R.id.sendCoinsBack -> {
-                    Log.i("Padalogs","clicked navigate to send coins back to faucet")
                     supportFragmentManager.beginTransaction().replace(R.id.fragmentAbout, SendCoinsBackFragment()).commit()
                     binding.drawerActivityLayout.closeDrawer(GravityCompat.START)
                     true
                 }
                 R.id.wallet -> {
-                    Log.i("Padalogs","clicked navigate back to wallet")
                     // startActivity(Intent(this, HomeActivity::class.java))
                     binding.drawerActivityLayout.closeDrawer(GravityCompat.START)
                     finish()
                     true
                 }
                 else -> {
-                    Log.i("Padalogs","Drawer selection didn't work properly ${it.itemId} was item ID")
                     true
                 }
             }
