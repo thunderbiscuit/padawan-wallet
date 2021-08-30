ade437d23209b9825068c2c8cc16a909412f61bc
Sunidhi Sharma <19ucs087@lnmiit.ac.in>
2021-08-30 17:57:10 +0530

    feat: Added tour to tutorial as well as the navigation menu in WalletActivity

--- app/src/main/java/com/goldenraven/padawanwallet/wallet/WalletActivity.kt
+++ app/src/main/java/com/goldenraven/padawanwallet/wallet/WalletActivity.kt
@@ -5,9 +5,12 @@
 
 package com.goldenraven.padawanwallet.wallet
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
@@ -18,10 +21,15 @@ import androidx.core.view.GravityCompat
 import androidx.viewpager2.widget.ViewPager2
 import com.goldenraven.padawanwallet.BuildConfig
 import com.goldenraven.padawanwallet.R
+import com.goldenraven.padawanwallet.data.Repository
 import com.goldenraven.padawanwallet.databinding.ActivityHomeBinding
 import com.goldenraven.padawanwallet.drawer.DrawerActivity
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import tourguide.tourguide.Overlay
+import tourguide.tourguide.Pointer
+import tourguide.tourguide.ToolTip
+import tourguide.tourguide.TourGuide
 
 class WalletActivity : AppCompatActivity() {
 
@@ -29,6 +37,9 @@ class WalletActivity : AppCompatActivity() {
     lateinit var tabLayout: TabLayout
     lateinit var viewPager: ViewPager2
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var tour : TourGuide
+    private lateinit var tourT : TourGuide
+    private lateinit var highlightedView: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,15 +52,39 @@ class WalletActivity : AppCompatActivity() {
         tabLayout = binding.tabLayout
         viewPager = binding.pager
 
+        val offerFaucetCallDone =
+                this.getSharedPreferences("current_wallet", Context.MODE_PRIVATE)
+                        ?.getBoolean("offerFaucetCallDone", false)
+
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             when (position) {
                 0 -> { tab.text = "Wallet" }
                 1 -> { tab.text = "Tutorials" }
             }
+            if (tab.text == "Wallet") {
+                if (offerFaucetCallDone == false) {
+                    tourT = TourGuide.init(this).with(TourGuide.Technique.Click)
+                            .setPointer(Pointer().setGravity(Gravity.RIGHT))
+                            .playOn(binding.tabLayout)
+                }
+            }
         }.attach()
 
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (offerFaucetCallDone == false)
+                    tourT.cleanUp()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
 
         val drawerToggle: ActionBarDrawerToggle = object : ActionBarDrawerToggle(
             this,
@@ -60,6 +95,13 @@ class WalletActivity : AppCompatActivity() {
             override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
                 super.onDrawerOpened(drawerView)
                 findViewById<TextView>(R.id.versionName).text = BuildConfig.VERSION_NAME
+            }
+
+            override fun onDrawerOpened(drawerView: View) {
+                super.onDrawerOpened(drawerView)
+                if (offerFaucetCallDone == false) {
+                    tour.cleanUp()
+                }
             }
         }
         binding.drawerLayout.addDrawerListener(drawerToggle)
@@ -74,7 +116,11 @@ class WalletActivity : AppCompatActivity() {
         val intentSeedPhrase: Intent = Intent(this, DrawerActivity::class.java).putExtra("drawerItem", "seedphrase")
         val intentSendCoinsBack: Intent = Intent(this, DrawerActivity::class.java).putExtra("drawerItem", "sendCoinsBack")
 
-
+        if (offerFaucetCallDone == false) {
+            tour = TourGuide.init(this).with(TourGuide.Technique.Click)
+                    .setPointer(Pointer().setGravity(Gravity.LEFT))
+                    .playOn(binding.toolbar)
+        }
         binding.navView.setNavigationItemSelectedListener {
             when(it.itemId) {
                 R.id.about -> {
@@ -131,4 +177,12 @@ class WalletActivity : AppCompatActivity() {
             super.onBackPressed()
         }
     }
+
+    private fun createTour(heading: String, message: String, button: Toolbar) {
+        tour = TourGuide.init(this).with(TourGuide.Technique.Click)
+                .setPointer(Pointer().setGravity(Gravity.LEFT))
+                .playOn(button)
+    }
 }
+
+
