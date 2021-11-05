b414460bb1df0757f8915ab4ffff6550319a9c8d
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2021-11-06 04:56:48 +0530

    Use new published package

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,7 +87,7 @@ dependencies {
     implementation("androidx.viewpager:viewpager:1.0.0")
 
     // bitcoin
-    implementation("org.bitcoindevkit:bdk-android:0.1.0")
+    implementation("org.bitcoindevkit:bdk-android:0.1.1")
 
     // Ktor
     implementation("io.ktor:ktor-client-cio:${Versions.ktor}")
