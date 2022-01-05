0be8c361ec93148221f500b90c1a02963d6350c8
thunderbiscuit <thunderbiscuit@protonmail.com>
2022-01-05 15:53:31 -0500

    Refactor: Use conventional test class name

--- app/src/test/java/com/goldenraven/padawanwallet/IsSendTest.kt
+++ app/src/test/java/com/goldenraven/padawanwallet/IsSendTest.kt
@@ -1,10 +1,15 @@
+/*
+ * Copyright 2020 thunderbiscuit and contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the ./LICENSE file.
+ */
+
 package com.goldenraven.padawanwallet
 
 import com.goldenraven.padawanwallet.utils.isSend
 import org.junit.Test
 import org.junit.Assert.*
 
-class TransactionSendOrReceive {
+class IsSendTest {
     @Test
     fun `transaction is a send`() {
         val isSend: Boolean = isSend(sent = 300, received = 20)
