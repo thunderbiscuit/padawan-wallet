fd248ab0b63db16b50a9480e2900f8ecc98a0193
thunderbiscuit <thunderbiscuit@protonmail.com>
2022-02-24 18:50:20 -0500

    Build: Bump dependencies to latest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ val faucetPassword: String by project
 
 android {
     compileSdk = 31
-    buildToolsVersion = "30.0.2"
+    buildToolsVersion = "30.0.3"
     ndkVersion = "21.4.7075529"
 
     buildFeatures {
@@ -77,12 +77,12 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.4.0")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.2")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("com.google.android.material:material:1.5.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.4.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.4.1")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.viewpager:viewpager:1.0.0")
@@ -99,9 +99,9 @@ dependencies {
     implementation("com.github.yuriy-budiyev:code-scanner:2.1.1")
 
     // Room
-    implementation("androidx.room:room-runtime:2.4.0")
-    implementation("androidx.room:room-ktx:2.3.0")
-    kapt("androidx.room:room-compiler:2.4.0")
+    implementation("androidx.room:room-runtime:2.4.2")
+    implementation("androidx.room:room-ktx:2.4.2")
+    kapt("androidx.room:room-compiler:2.4.2")
 
     // testing
     testImplementation("junit:junit:4.13.2")
