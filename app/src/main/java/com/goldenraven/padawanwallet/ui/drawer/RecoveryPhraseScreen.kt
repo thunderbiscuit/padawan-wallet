0525f4dc278c581c5b8ff3272fd1b0634ec52e8f
Tang Yetong <tangyetong666@gmail.com>
2022-04-11 15:16:26 +0800

    Feat: Added 2 drawer screens

--- app/src/main/java/com/goldenraven/padawanwallet/ui/drawer/RecoveryPhraseScreen.kt
+++ app/src/main/java/com/goldenraven/padawanwallet/ui/drawer/RecoveryPhraseScreen.kt
@@ -1,15 +1,37 @@
 package com.goldenraven.padawanwallet.ui.wallet
 
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.goldenraven.padawanwallet.data.Repository
 import com.goldenraven.padawanwallet.ui.DrawerAppBar
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun RecoveryPhraseScreen(navController: NavController) {
+
+    val seedPhrase: String = Repository.getMnemonic()
+    val wordList: List<String> = seedPhrase.split(" ")
+
     Scaffold(
         topBar = { DrawerAppBar(navController, title = "Wallet Recovery Phrase") },
-    ) { }
+    ) {
+        Column (
+            modifier = Modifier.padding(all = 32.dp)
+        ){
+            wordList.forEachIndexed { index, item ->
+                Text(
+                    text = "${index + 1}: $item",
+                    modifier = Modifier.weight(weight = 1F)
+                )
+            }
+        }
+    }
 }
