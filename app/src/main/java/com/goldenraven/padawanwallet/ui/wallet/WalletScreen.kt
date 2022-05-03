e831dd2fffd0d72f6802e23fbcf14bb20a8ef33d
thunderbiscuit <thunderbiscuit@protonmail.com>
2022-05-03 12:07:33 -0400

    Add UI to WalletScreen

--- app/src/main/java/com/goldenraven/padawanwallet/ui/wallet/WalletScreen.kt
+++ app/src/main/java/com/goldenraven/padawanwallet/ui/wallet/WalletScreen.kt
@@ -1,22 +1,34 @@
 package com.goldenraven.padawanwallet.ui.wallet
 
+import com.goldenraven.padawanwallet.R
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.goldenraven.padawanwallet.theme.md_theme_dark_background
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.LiveData
 import androidx.navigation.NavHostController
 import com.goldenraven.padawanwallet.data.Wallet
+import com.goldenraven.padawanwallet.theme.*
 import com.goldenraven.padawanwallet.ui.Screen
+import com.goldenraven.padawanwallet.utils.formatInBtc
 
 @Composable
 internal fun WalletScreen(
@@ -24,26 +36,78 @@ internal fun WalletScreen(
     navController: NavHostController
 ) {
     val balance by walletViewModel.balance.observeAsState()
+    // walletViewModel.updateBalance()
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(md_theme_dark_background)
     ) {
-        Text(text = "The balance is $balance")
-        Button(
-            onClick = { walletViewModel.updateBalance() },
+        Spacer(Modifier.padding(24.dp))
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .background(color = md_theme_dark_background2)
+                .height(110.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
         ) {
-            Text(text = "Balance")
+            Image(
+                painter = painterResource(id = R.drawable.sat),
+                contentDescription = "Bitcoin testnet logo",
+                Modifier
+                    .align(Alignment.CenterVertically)
+                    .size(60.dp)
+            )
+            Text(
+                balance.toString(),
+                fontFamily = shareTechMono,
+                fontSize = 32.sp,
+            )
         }
-        Button(
-            onClick = { navController.navigate(Screen.SendScreen.route) },
+        // Spacer(Modifier.padding(24.dp))
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.height(140.dp).fillMaxWidth().padding(horizontal = 16.dp)
         ) {
-            Text(text = "Send")
-        }
-        Button(
-            onClick = { navController.navigate(Screen.ReceiveScreen.route) },
-        ) {
-            Text(text = "Receive")
+            Button(
+                onClick = { navController.navigate(Screen.SendScreen.route) },
+                colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.secondary),
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier
+                    .height(80.dp)
+                    .padding(vertical = 8.dp, horizontal = 8.dp)
+                    .shadow(elevation = 4.dp, shape = RoundedCornerShape(16.dp))
+            ) {
+                Text(
+                    text = "Send",
+                    fontFamily = jost,
+                    fontSize = 18.sp,
+                    textAlign = TextAlign.Center,
+                    lineHeight = 28.sp,
+                    modifier = Modifier
+                        .fillMaxWidth(0.4f)
+                )
+            }
+            Button(
+                onClick = { navController.navigate(Screen.ReceiveScreen.route) },
+                colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.primary),
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier
+                    .height(80.dp)
+                    .padding(vertical = 8.dp, horizontal = 8.dp)
+                    .shadow(elevation = 4.dp, shape = RoundedCornerShape(16.dp))
+            ) {
+                Text(
+                    text = "Receive",
+                    fontFamily = jost,
+                    fontSize = 18.sp,
+                    textAlign = TextAlign.Center,
+                    lineHeight = 28.sp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+            }
         }
     }
 }
