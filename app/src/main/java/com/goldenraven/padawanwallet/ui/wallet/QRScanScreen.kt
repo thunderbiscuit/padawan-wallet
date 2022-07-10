a29686a0ead4ef2feb992cfb01a14149cd64c549
thunderbiscuit <thunderbiscuit@protonmail.com>
2022-07-10 11:05:09 -0400

    Fix: Fix warnings

--- app/src/main/java/com/goldenraven/padawanwallet/ui/wallet/QRScanScreen.kt
+++ app/src/main/java/com/goldenraven/padawanwallet/ui/wallet/QRScanScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.core.content.ContextCompat
