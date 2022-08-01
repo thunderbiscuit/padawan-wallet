e4d4de4dd279279c86273de710a19fb8284f3e13
thunderbiscuit <thunderbiscuit@protonmail.com>
2022-08-01 14:33:58 -0400

    UI: Add Outfit font

--- app/src/main/java/com/goldenraven/padawanwallet/theme/Fonts.kt
+++ app/src/main/java/com/goldenraven/padawanwallet/theme/Fonts.kt
@@ -6,6 +6,11 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import com.goldenraven.padawanwallet.R
 
+val Outfit = FontFamily(
+    Font(R.font.outfit_regular, FontWeight.Normal),
+    Font(R.font.outfit_semibold, FontWeight.SemiBold),
+)
+
 val ShareTechMono = FontFamily(
     Font(R.font.share_tech_mono, FontWeight.Normal),
 )
