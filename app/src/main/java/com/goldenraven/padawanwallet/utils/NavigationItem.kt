df5f95e3abbe50c5e484838b76a994722b8fef10
thunderbiscuit <thunderbiscuit@protonmail.com>
2022-09-02 13:56:08 -0400

    Feat: Add Hicon icons to bottom navigation bar

--- app/src/main/java/com/goldenraven/padawanwallet/utils/NavigationItem.kt
+++ app/src/main/java/com/goldenraven/padawanwallet/utils/NavigationItem.kt
@@ -4,7 +4,7 @@ import com.goldenraven.padawanwallet.R
 import com.goldenraven.padawanwallet.ui.Screen
 
 sealed class NavigationItem(val route: String, val icon_filled: Int, val icon_outline: Int, val title: String) {
-    object Home : NavigationItem(route = Screen.WalletScreen.route, icon_filled = R.drawable.ic_home, icon_outline = R.drawable.ic_home_outline, title = "Home")
-    object Tutorial : NavigationItem(route = Screen.TutorialsHomeScreen.route, icon_filled = R.drawable.ic_school, icon_outline = R.drawable.ic_school_outline, title = "Learn")
-    object Settings : NavigationItem(route = Screen.SettingsScreen.route, icon_filled = R.drawable.ic_settings, icon_outline = R.drawable.ic_settings_outline, title = "Settings")
+    object Home : NavigationItem(route = Screen.WalletScreen.route, icon_filled = R.drawable.ic_hicon_wallet, icon_outline = R.drawable.ic_hicon_wallet, title = "Wallet")
+    object Tutorial : NavigationItem(route = Screen.TutorialsHomeScreen.route, icon_filled = R.drawable.ic_hicon_education, icon_outline = R.drawable.ic_hicon_education, title = "Learn")
+    object Settings : NavigationItem(route = Screen.SettingsScreen.route, icon_filled = R.drawable.ic_hicon_menu, icon_outline = R.drawable.ic_hicon_menu, title = "Menu")
 }
