26f8a41fa5cedc11e45813049ff44eb64404978c
thunderbiscuit <thunderbiscuit@protonmail.com>
2023-11-16 21:23:29 -0500

    Refactor: Remove unused colours

--- androidApp/src/main/java/com/goldenraven/padawanwallet/theme/Color.kt
+++ androidApp/src/main/java/com/goldenraven/padawanwallet/theme/Color.kt
@@ -7,39 +7,11 @@ package com.goldenraven.padawanwallet.theme
 
 import androidx.compose.ui.graphics.Color
 
-// val md_theme_dark_primary = Color(0xFFb8bb26)
-// val md_theme_dark_onPrimary = Color(0xFFebdbb2)
-// val md_theme_dark_primaryContainer = Color(0xFFf50057) // pink
-// val md_theme_dark_onPrimaryContainer = Color(0xFFf50057) // pink
-// val md_theme_dark_secondary = Color(0xFFd65d0e)
-// val md_theme_dark_onSecondary = Color(0xFFf50057) // pink
-// val md_theme_dark_secondaryContainer = Color(0xFFf50057) // pink
-// val md_theme_dark_onSecondaryContainer = Color(0xFFf50057) // pink
-// val md_theme_dark_tertiary = Color(0xFFf50057) // pink
-// val md_theme_dark_onTertiary = Color(0xFFf50057) // pink
-// val md_theme_dark_tertiaryContainer = Color(0xFFf50057) // pink
-// val md_theme_dark_onTertiaryContainer = Color(0xFFf50057) // pink
-// val md_theme_dark_error = Color(0xFFcc241d)
-// val md_theme_dark_errorContainer = Color(0xFFf50057) // pink
-// val md_theme_dark_onError = Color(0xFFf50057) // pink
-// val md_theme_dark_onErrorContainer = Color(0xFFf50057) // pink
 val md_theme_dark_background = Color(0xFF32302f)
-val md_theme_dark_onBackground = Color(0xFFebdbb2)
-val md_theme_dark_surface = Color(0xFF504945)
-// val md_theme_dark_onSurface = Color(0xFFf50057) // pink
-// val md_theme_dark_surfaceVariant = Color(0xFFf50057) // pink
-// val md_theme_dark_onSurfaceVariant = Color(0xFFf50057) // pink
-// val md_theme_dark_outline = Color(0xFFf50057) // pink
-// val md_theme_dark_inverseOnSurface = Color(0xFFf50057) // pink
-// val md_theme_dark_inverseSurface = Color(0xFFf50057) // pink
-// val md_theme_dark_inversePrimary = Color(0xFFf50057) // pink
-// val md_theme_dark_shadow = Color(0xFFf50057) // pink
-//
-// val seed = Color(0xFFf50057) // pink
+
 val padawan_theme_primary = Color(0xFF76dab3)
 val padawan_theme_onPrimary = Color(0xFF000000)
 val padawan_theme_background = Color(0xFF76dab3)
-val padawan_theme_onBackground = Color(0xFF8480e5)
 val padawan_theme_background_secondary = Color(0xFFf3f4ff)
 val padawan_theme_onBackground_secondary = Color(0xFFdadeff)
 val padawan_theme_onBackground_faded = Color(0x668480e5)
@@ -49,31 +21,22 @@ val padawan_disabled = Color(0xA6000000)
 val padawan_theme_receive_primary = Color(0x668480e5)
 val padawan_theme_send_primary = Color(0x6676dab3)
 val padawan_theme_navigation_bar_unselected = Color(0xFF8a8a8a)
-val padawan_theme_progressbar_background = Color(0x4dd3d3d3)
 
 val padawan_theme_text_headline = Color(0xff1f0208) // default dark text (not quite black)
 val padawan_theme_text_default = Color(0xFF000000)
 val padawan_theme_text_faded = Color(0x60000000)
 val padawan_theme_text_faded_secondary = Color(0xff787878)
 
-val padawan_theme_chapter_completed = Color(0xffffc847)
-val padawan_theme_tutorial_faded = Color(0xffffE6aa)
 val padawan_theme_tutorial_background = Color(0xfff6cf47)
-// val padawan_theme_tutorial_background = Color(0xFFBA4FFC)
-val padawan_theme_tutorial_chapter = Color(0xffcc241d)
-
 val padawan_theme_button_primary = Color(0xffff6d3b)
 val padawan_theme_button_secondary = Color(0xffffd1bf)
 
 val white = Color(0xffffffff)
 val error = Color(0xffcc241d)
 
-val md_theme_dark_surfaceLight = Color(0xff504945)
 val md_theme_dark_onBackgroundFaded = Color(0x80ebdbb2)
-val md_theme_dark_lightBackground = Color(0xfff2e5bc)
 val md_theme_dark_onLightBackground = Color(0xff282828)
 val md_theme_dark_warning = Color(0xfffabd2f)
-val md_theme_dark_background2 = Color(0xff3c3836)
 
 val connection_available = Color(0xff6FCF97)
 val connection_unavailable = Color(0xffEB5757)
--- androidApp/src/main/java/com/goldenraven/padawanwallet/theme/Theme.kt
+++ androidApp/src/main/java/com/goldenraven/padawanwallet/theme/Theme.kt
@@ -8,15 +8,10 @@ package com.goldenraven.padawanwallet.theme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 
 private val PadawanThemeColors = lightColorScheme(
     primary = padawan_theme_primary,
     onPrimary = padawan_theme_onPrimary,
-    // background = padawan_theme_background,
-    // onBackground = padawan_theme_onBackground,
 )
 
 @Composable
