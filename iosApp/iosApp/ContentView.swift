092be75b93c6daaaa601584c88478cb0b0e36102
geigerzaehler242 <geigerzaehler242@users.noreply.github.com>
2023-12-05 14:36:29 -0500

    Feat: Add simple screens and viewmodel to iOS app
    - update supported ios version and xcode version
    - add BDK framework to project
    - add NoJDK target to be able to build without Gradle/JVK
    - add tab bar navigation
    - add example/test use of BDK to show block height
    - add some test buttons and test UI to third tab
    - Refactor: change target name from "iosApp copy" to iosAppNoJDK

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,16 +1,154 @@
 import SwiftUI
-import SharedPadawan
+//import SharedPadawan
 
 struct ContentView: View {
-	let greet = Greeting().greet()
+    
+    // MARK: PROPERTIES
+    @State var selectedTab: Int = 0
+//	let greet = Greeting().greet()
 
+    enum Tab: Int {
+        case firstTab = 0, secondTab, thirdTab, fourthTab
+    }
+    
+    // MARK: BODY
 	var body: some View {
-		Text(greet)
+
+        TabView(selection: $selectedTab) {
+                        
+            WalletView(selectedTab: $selectedTab)
+                .tabItem {
+                    Image(systemName: "bitcoinsign.square.fill")
+                    Text("Wallet")
+                }
+                .tag(Tab.firstTab.rawValue)
+            
+            LearningView()
+                .font(.largeTitle)
+                .foregroundColor(.blue)
+                .tabItem {
+                    Image(systemName: "graduationcap.fill")
+                    Text("Learn")
+                }
+                .tag(Tab.secondTab.rawValue)
+            
+            MenuView()
+                .font(.largeTitle)
+                .foregroundColor(.blue)
+                .tabItem{
+                    Image(systemName: "text.justify.trailing")
+                    Text("Menu")
+                }
+                .tag(Tab.thirdTab.rawValue)
+            
+        }
+        
+        
 	}
 }
+
+
+struct WalletView: View {
+    
+    @EnvironmentObject var viewModel: WalletViewModel
+    @Binding var selectedTab: Int
+    
+    var body: some View {
+        ZStack {
+            //Color.red.ignoresSafeArea()
+            VStack {
+                
+                Image("Logo")
+                Spacer()
+                
+                
+                Text("Block Height")
+                    .font(.largeTitle)
+                    .foregroundColor(.blue)
+                
+                Text("\(String(viewModel.blockHeight))")
+                    //.font(.largeTitle)
+                    .font(.system(size: 60))
+                    .foregroundColor(.blue)
+                
+
+                Spacer()
+                
+            }
+        }
+        .onAppear(perform: viewModel.load)
+    }
+}
+struct LearningView: View {
+    
+    @State var isSelected: Bool = false
+    
+    var body: some View {
+        
+        VStack(spacing: 40) {
+            
+            Text("Learning Tab")
+            
+            Spacer()
+        }
+        .padding(40)
+        
+    }
+}
+
+struct MenuView: View {
+    
+    @State var isSelected: Bool = false
+    
+    var body: some View {
+        
+        VStack(spacing: 40) {
+            
+            RoundedRectangle(cornerRadius: 25)
+                .frame(height: 200)
+                .foregroundColor(isSelected ? Color.green : Color.red)
+            
+            Button(action: {
+                isSelected.toggle()
+            }, label: {
+                Text("Button")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(25)
+            })
+            
+            Text("Double TAP Gesture")
+                .font(.headline)
+                .foregroundColor(.white)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+                .background(Color.blue)
+                .cornerRadius(25)
+//                .onTapGesture {
+//                    isSelected.toggle()
+//                }
+                .onTapGesture(count: 2, perform: {
+                    isSelected.toggle()
+                })
+            
+            Text("Menu Item 1")
+            Text("Menu Item 2")
+            
+            Spacer()
+        }
+        .padding(40)
+        
+    }
+}
+
+// MARK: PREVIEW
 
 struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
 		ContentView()
+            .environmentObject(WalletViewModel())
 	}
 }
--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -5,6 +5,7 @@ struct iOSApp: App {
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
+                .environmentObject(WalletViewModel())
 		}
 	}
 }
