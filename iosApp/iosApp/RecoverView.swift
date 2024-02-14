8ed30ec1f32b94dedccf950d1e4c0764208a1ad5
thunderbiscuit <thunderbiscuit@protonmail.com>
2024-02-14 15:33:39 -0500

    Docs: Fix license notice on Swift files

--- iosApp/iosApp/RecoverView.swift
+++ iosApp/iosApp/RecoverView.swift
@@ -2,8 +2,8 @@
 //  RecoverView.swift
 //  iOSApp
 //
-// Copyright 2024 thunderbiscuit, geigerzaehler, and contributors.
-// Use of this source code is governed by the Apache 2.0 license that can be found in the ./LICENSE file.
+//  Copyright 2024 thunderbiscuit, geigerzaehler, and contributors.
+//  Use of this source code is governed by the Apache 2.0 license that can be found in the ./LICENSE.txt file.
 //
 
 import SwiftUI
