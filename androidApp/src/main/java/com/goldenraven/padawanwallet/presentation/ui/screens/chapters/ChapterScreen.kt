43aa691d916141791d1c60557128ec36e5445c61
thunderbiscuit <thunderbiscuit@protonmail.com>
2024-05-01 22:28:34 -0400

    Update license notice in source files

--- androidApp/src/main/java/com/goldenraven/padawanwallet/presentation/ui/screens/chapters/ChapterScreen.kt
+++ androidApp/src/main/java/com/goldenraven/padawanwallet/presentation/ui/screens/chapters/ChapterScreen.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2023 thunderbiscuit and contributors.
+ * Copyright 2020-2024 thunderbiscuit and contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the ./LICENSE.txt file.
  */
 
--- androidApp/src/main/java/com/goldenraven/padawanwallet/presentation/viewmodels/ChaptersViewModel.kt
+++ androidApp/src/main/java/com/goldenraven/padawanwallet/presentation/viewmodels/ChaptersViewModel.kt
@@ -1,4 +1,8 @@
-package com.goldenraven.padawanwallet.viewmodels
+/*
+ * Copyright 2020-2024 thunderbiscuit and contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the ./LICENSE file.
+ */
+
 package com.goldenraven.padawanwallet.presentation.viewmodels
 
 import android.app.Application
