db1ceb30a511433ce2c5aec4a5de0b42d90e90cb
thunderbiscuit <thunderbiscuit@protonmail.com>
2024-07-22 13:30:23 -0400

    Chore: Bump Android Gradle Plugin to 8.5.0 and Gradle wrapper to 8.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application").version("8.2.0-rc03").apply(false)
-    id("com.android.library").version("8.2.0-rc03").apply(false)
+    id("com.android.application").version("8.5.0").apply(false)
+    id("com.android.library").version("8.5.0").apply(false)
     id("org.jetbrains.kotlin.android").version("1.9.20").apply(false)
     id("org.jetbrains.kotlin.multiplatform").version("1.9.20").apply(false)
     id("com.google.devtools.ksp").version("1.9.20-1.0.14").apply(false)
