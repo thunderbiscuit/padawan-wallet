51ed4d258640f22f6faa783e8b1c6926226413e1
thunderbiscuit <thunderbiscuit@protonmail.com>
2024-09-14 07:40:51 -0400

    Chore: Bump version to 0.14.0

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.goldenraven.padawanwallet"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "v0.14.0-SNAPSHOT"
+        versionCode = 20
+        versionName = "v0.14.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         resValue(
