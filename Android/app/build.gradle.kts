928d7704855dc73ba676a3d3b465159c561549ec
thunderbiscuit <thunderbiscuit@protonmail.com>
2025-05-26 15:11:31 -0400

    Update build script for Android App Bundles

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -34,9 +34,15 @@ android {
         targetSdk = 35
         // This version code must be updated for each release but the version on the master branch
         // stays at 1.
-        versionCode = 2
+        versionCode = 3
         versionName = "v0.16.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        // NOTE: This cannot be configured at the same time as the splits block when building App Bundles. Comment out
+        //       the this block and uncomment the splits block when building APKs.
+        ndk {
+            abiFilters += "arm64-v8a"
+        }
 
         resValue(
             type = "string",
@@ -75,14 +81,16 @@ android {
         }
     }
 
-    splits {
-        abi {
-            isEnable = true
-            reset()
-            include("arm64-v8a")
-            isUniversalApk = false
-        }
-    }
+    // NOTE: This cannot be configured at the same time as the ndk block when building App Bundles. Comment out the ndk
+    //       block and uncomment this block when building APKs.
+    // splits {
+    //     abi {
+    //         isEnable = true
+    //         reset()
+    //         include("arm64-v8a")
+    //         isUniversalApk = false
+    //     }
+    // }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
