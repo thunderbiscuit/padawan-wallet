ad02dc6445c48ab4a268600fe841dfb03a47a1a2
thunderbiscuit <thunderbiscuit@protonmail.com>
2025-06-24 19:32:53 -0400

    [Android] Upgrade AGP and Android library plugins

--- Android/build.gradle.kts
+++ Android/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application").version("8.6.0").apply(false)
-    id("com.android.library").version("8.6.0").apply(false)
+    id("com.android.application").version("8.9.1").apply(false)
+    id("com.android.library").version("8.9.1").apply(false)
     id("org.jetbrains.kotlin.android").version("2.1.20").apply(false)
     id("org.jetbrains.kotlin.multiplatform").version("2.1.20").apply(false)
     id("org.jetbrains.kotlin.plugin.serialization").version("2.1.20").apply(false)
