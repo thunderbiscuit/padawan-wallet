1975bbfb9740900b3cb7aa0ca47ca6eaff724d2c
Rubens <r1b2ns@duck.com>
2025-07-14 20:56:22 -0300

    Feat(iOS): Added fonts from Android project

---
+++ iOS/PadawanWallet/Resources/Fonts/Fonts+Util.swift
@@ -0,0 +1,34 @@
+//
+//  Fonts+Util.swift
+//  PadawanWallet
+//
+//  Created by Rubens Machion on 18/07/25.
+//
+
+import SwiftUI
+
+extension Fonts {
+    static func font(
+        _ weight: SwiftUI.Font.Weight = .regular,
+        _ size: CGFloat = 14.0
+    ) -> SwiftUI.Font {
+        switch weight {
+        case .bold, .semibold:
+            return Fonts.Outfit.semiBold.swiftUIFont(size: size)
+
+        case .medium:
+            return Fonts.Outfit.medium.swiftUIFont(size: size)
+
+        default:
+            return Fonts.Outfit.regular.swiftUIFont(size: size)
+        }
+    }
+    
+    static var title: SwiftUI.Font {
+        Fonts.font(.bold, 32)
+    }
+    
+    static var subtitle: SwiftUI.Font {
+        Fonts.font(.regular, 18)
+    }
+}
