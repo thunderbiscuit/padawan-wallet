284f0dcba299ffbba9ff39f25c81222b8134bfdf
Vini <vinnismoreira@icloud.com>
2025-09-05 13:03:09 -0300

    Refatoring TabView and new Image component.

---
+++ iOS/PadawanWallet/UI Components/Card/PadawanCardIconView.swift
@@ -0,0 +1,18 @@
+//
+//  PadawanCardIconView.swift
+//  PadawanWallet
+//
+//  Created by Vinicius Silva Moreira on 05/09/25.
+//
+
+import SwiftUI
+
+struct PadawanCardIconView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PadawanCardIconView()
+}
