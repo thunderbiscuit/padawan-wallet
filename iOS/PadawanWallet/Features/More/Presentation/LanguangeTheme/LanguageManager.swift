6ce3e64e706a11d4728e4e8be57a028b57932984
Vini <vinnismoreira@icloud.com>
2025-10-14 14:54:31 -0300

    Add new component for manage languages with no restart and injection component in App.

---
+++ iOS/PadawanWallet/Features/More/Presentation/LanguangeTheme/LanguageManager.swift
@@ -0,0 +1,52 @@
+//
+//  LanguageManager.swift
+//  PadawanWallet
+//
+//  Created by Vinicius Silva Moreira on 14/10/25.
+//
+
+import Foundation
+import Combine
+
+class LanguageManager: ObservableObject {
+    
+    static let shared = LanguageManager()
+    
+    @Published var currentLanguage: PadawanLanguage {
+        didSet {
+            UserDefaults.standard.setValue(currentLanguage.code, forKey: "selectedLanguage")
+            updateBundle()
+        }
+    }
+    
+    private var bundle: Bundle?
+
+    private init() {
+        if let savedLangCode = UserDefaults.standard.string(forKey: "selectedLanguage"),
+           let savedLang = PadawanLanguage.allCases.first(where: { $0.code == savedLangCode }) {
+            self.currentLanguage = savedLang
+        } else {
+            let deviceLanguage = Bundle.main.preferredLocalizations.first ?? "en"
+            self.currentLanguage = PadawanLanguage.allCases.first(where: { $0.code == deviceLanguage }) ?? .english
+        }
+        
+        updateBundle()
+    }
+    
+    func setLanguage(_ language: PadawanLanguage) {
+        currentLanguage = language
+    }
+
+    func localizedString(forKey key: String) -> String {
+        return bundle?.localizedString(forKey: key, value: nil, table: nil) ?? key
+    }
+    
+    private func updateBundle() {
+        guard let path = Bundle.main.path(forResource: currentLanguage.code, ofType: "lproj"),
+              let newBundle = Bundle(path: path) else {
+            self.bundle = Bundle.main
+            return
+        }
+        self.bundle = newBundle
+    }
+}
