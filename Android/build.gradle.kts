c1854eaab0a326163baebb9b710ac2d7a430993d
thunderbiscuit <thunderbiscuit@protonmail.com>
2026-01-19 13:40:34 -0500

    [Android] Bump Kotlin version to 2.3.0

--- Android/build.gradle.kts
+++ Android/build.gradle.kts
@@ -1,8 +1,9 @@
+// All versions of the android, multiplatform, serialization, and compose plugins should be the same
+
 plugins {
     id("com.android.application").version("8.9.1").apply(false)
     id("com.android.library").version("8.9.1").apply(false)
-    id("org.jetbrains.kotlin.android").version("2.1.20").apply(false)
-    id("org.jetbrains.kotlin.multiplatform").version("2.1.20").apply(false)
-    id("org.jetbrains.kotlin.plugin.serialization").version("2.1.20").apply(false)
-    id("org.jetbrains.kotlin.plugin.compose").version("2.1.20").apply(false)
+    id("org.jetbrains.kotlin.android").version("2.3.0").apply(false)
+    id("org.jetbrains.kotlin.plugin.serialization").version("2.3.0").apply(false)
+    id("org.jetbrains.kotlin.plugin.compose").version("2.3.0").apply(false)
 }
